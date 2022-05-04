17f042112dc69e0c973108b03fc07c5ec639c074
Bekir Ozturk <essoperagma@gmail.com>
2022-05-04 23:29:51 +0200

    Added EmbeddedBufferData scenario

--- StructWrapper/Benchmarks.cs
+++ StructWrapper/Benchmarks.cs
@@ -35,21 +35,21 @@ namespace StructWrapper
         {
             public MethodHost_Span WriteInt32_Span(ref Span<byte> span, int value)
             {
-                MemoryMarshal.Cast<byte, int>(span)[0] = value;
+                MemoryMarshal.Write(span, ref value);
                 span = span.Slice(sizeof(int));
                 return default(MethodHost_Span);
             }
 
             public MethodHost_Span WriteSingle_Span(ref Span<byte> span, float value)
             {
-                MemoryMarshal.Cast<byte, float>(span)[0] = value;
+                MemoryMarshal.Write(span, ref value);
                 span = span.Slice(sizeof(float));
                 return default(MethodHost_Span);
             }
 
             public MethodHost_Span WriteDouble_Span(ref Span<byte> span, double value)
             {
-                MemoryMarshal.Cast<byte, double>(span)[0] = value;
+                MemoryMarshal.Write(span, ref value);
                 span = span.Slice(sizeof(double));
                 return default(MethodHost_Span);
             }
@@ -66,23 +66,53 @@ namespace StructWrapper
         {
             public MethodHost_WrappedSpan WriteInt32_WrappedSpan(ref WrappedSpan wrapper, int value)
             {
-                MemoryMarshal.Cast<byte, int>(wrapper.Span)[0] = value;
+                MemoryMarshal.Write(wrapper.Span, ref value);
                 wrapper.Span = wrapper.Span.Slice(sizeof(int));
                 return default(MethodHost_WrappedSpan);
             }
 
             public MethodHost_WrappedSpan WriteSingle_WrappedSpan(ref WrappedSpan wrapper, float value)
             {
-                MemoryMarshal.Cast<byte, float>(wrapper.Span)[0] = value;
+                MemoryMarshal.Write(wrapper.Span, ref value);
                 wrapper.Span = wrapper.Span.Slice(sizeof(float));
                 return default(MethodHost_WrappedSpan);
             }
 
             public MethodHost_WrappedSpan WriteDouble_WrappedSpan(ref WrappedSpan wrapper, double value)
             {
-                MemoryMarshal.Cast<byte, double>(wrapper.Span)[0] = value;
+                MemoryMarshal.Write(wrapper.Span, ref value);
                 wrapper.Span = wrapper.Span.Slice(sizeof(double));
                 return default(MethodHost_WrappedSpan);
+            }
+        }
+
+        public readonly ref struct MethodHost_EmbeddedBufferData
+        {
+            public readonly Span<byte> End;
+            public readonly Memory<byte> Begin;
+
+            public MethodHost_EmbeddedBufferData(Memory<byte> begin, Span<byte> end)
+            {
+                Begin = begin;
+                End = Begin.Span;
+            }
+
+            public MethodHost_EmbeddedBufferData WriteInt32(int value)
+            {
+                MemoryMarshal.Write(End, ref value);
+                return new MethodHost_EmbeddedBufferData(Begin, End.Slice(sizeof(int)));
+            }
+
+            public MethodHost_EmbeddedBufferData WriteSingle(float value)
+            {
+                MemoryMarshal.Write(End, ref value);
+                return new MethodHost_EmbeddedBufferData(Begin, End.Slice(sizeof(float)));
+            }
+
+            public MethodHost_EmbeddedBufferData WriteDouble(double value)
+            {
+                MemoryMarshal.Write(End, ref value);
+                return new MethodHost_EmbeddedBufferData(Begin, End.Slice(sizeof(double)));
             }
         }
         #endregion
@@ -133,6 +163,26 @@ namespace StructWrapper
         }
 
         [Benchmark]
+        public void WriteMany_Int32_EmbeddedBufferData()
+        {
+            MethodHost_EmbeddedBufferData data = new MethodHost_EmbeddedBufferData(targetBuffer, targetBuffer);
+
+            for (int i = 0; i < iterationCount; i += 10)
+            {
+                data = data.WriteInt32(sampleIntValues[i])
+                        .WriteInt32(sampleIntValues[i + 1])
+                        .WriteInt32(sampleIntValues[i + 2])
+                        .WriteInt32(sampleIntValues[i + 3])
+                        .WriteInt32(sampleIntValues[i + 4])
+                        .WriteInt32(sampleIntValues[i + 5])
+                        .WriteInt32(sampleIntValues[i + 6])
+                        .WriteInt32(sampleIntValues[i + 7])
+                        .WriteInt32(sampleIntValues[i + 8])
+                        .WriteInt32(sampleIntValues[i + 9]);
+            }
+        }
+
+        [Benchmark]
         public void WriteMany_Single_Span()
         {
             Span<byte> span = targetBuffer;
@@ -174,6 +224,27 @@ namespace StructWrapper
                     .WriteSingle_WrappedSpan(ref wrappedSpan, sampleFloatValues[i + 7])
                     .WriteSingle_WrappedSpan(ref wrappedSpan, sampleFloatValues[i + 8])
                     .WriteSingle_WrappedSpan(ref wrappedSpan, sampleFloatValues[i + 9]);
+            }
+        }
+
+        [Benchmark]
+        public void WriteMany_Single_EmbeddedBufferData()
+        {
+            MethodHost_EmbeddedBufferData data = new MethodHost_EmbeddedBufferData(targetBuffer, targetBuffer);
+
+            for (int i = 0; i < iterationCount; i += 10)
+            {
+                data = data
+                        .WriteSingle(sampleFloatValues[i])
+                        .WriteSingle(sampleFloatValues[i + 1])
+                        .WriteSingle(sampleFloatValues[i + 2])
+                        .WriteSingle(sampleFloatValues[i + 3])
+                        .WriteSingle(sampleFloatValues[i + 4])
+                        .WriteSingle(sampleFloatValues[i + 5])
+                        .WriteSingle(sampleFloatValues[i + 6])
+                        .WriteSingle(sampleFloatValues[i + 7])
+                        .WriteSingle(sampleFloatValues[i + 8])
+                        .WriteSingle(sampleFloatValues[i + 9]);
             }
         }
 
@@ -223,6 +294,27 @@ namespace StructWrapper
         }
 
         [Benchmark]
+        public void WriteMany_Double_EmbeddedBufferData()
+        {
+            MethodHost_EmbeddedBufferData data = new MethodHost_EmbeddedBufferData(targetBuffer, targetBuffer);
+
+            for (int i = 0; i < iterationCount; i += 10)
+            {
+                data = data
+                        .WriteDouble(sampleDoubleValues[i])
+                        .WriteDouble(sampleDoubleValues[i + 1])
+                        .WriteDouble(sampleDoubleValues[i + 2])
+                        .WriteDouble(sampleDoubleValues[i + 3])
+                        .WriteDouble(sampleDoubleValues[i + 4])
+                        .WriteDouble(sampleDoubleValues[i + 5])
+                        .WriteDouble(sampleDoubleValues[i + 6])
+                        .WriteDouble(sampleDoubleValues[i + 7])
+                        .WriteDouble(sampleDoubleValues[i + 8])
+                        .WriteDouble(sampleDoubleValues[i + 9]);
+            }
+        }
+
+        [Benchmark]
         public void WriteMany_Mixed_Span()
         {
             Span<byte> span = targetBuffer;
@@ -264,6 +356,27 @@ namespace StructWrapper
                     .WriteSingle_WrappedSpan(ref wrappedSpan, sampleIntValues[i + 7])
                     .WriteDouble_WrappedSpan(ref wrappedSpan, sampleDoubleValues[i + 8])
                     .WriteInt32_WrappedSpan(ref wrappedSpan, sampleIntValues[i + 9]);
+            }
+        }
+
+        [Benchmark]
+        public void WriteMany_Mixed_EmbeddedBufferData()
+        {
+            MethodHost_EmbeddedBufferData data = new MethodHost_EmbeddedBufferData(targetBuffer, targetBuffer);
+
+            for (int i = 0; i < iterationCount; i += 10)
+            {
+                data = data
+                        .WriteInt32(sampleIntValues[i])
+                        .WriteSingle(sampleFloatValues[i + 1])
+                        .WriteDouble(sampleDoubleValues[i + 2])
+                        .WriteInt32(sampleIntValues[i + 3])
+                        .WriteSingle(sampleFloatValues[i + 4])
+                        .WriteDouble(sampleDoubleValues[i + 5])
+                        .WriteInt32(sampleIntValues[i + 6])
+                        .WriteSingle(sampleFloatValues[i + 7])
+                        .WriteDouble(sampleDoubleValues[i + 8])
+                        .WriteInt32(sampleIntValues[i + 9]);
             }
         }
     }
